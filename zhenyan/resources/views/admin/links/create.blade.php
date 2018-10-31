34836c9c2446c9e1c9faf8c1cd856c6af44d57d2
zyx729451014 <zyx729451014@163.com>
2018-10-31 21:50:24 +0800

    用户管理 友情链接 类别管理

--- zhenyan/resources/views/admin/links/create.blade.php
+++ zhenyan/resources/views/admin/links/create.blade.php
@@ -1,46 +1,57 @@
 @extends('admin/layout/layout');
 @section('content-wrapper')
-<div class="col-md-10 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">友情链接添加</h4>
-                  <form class="forms-sample" action="/admin/links" method="post">
-                  {{ csrf_field() }}
-                    <div class="form-group row">
-                      <label for="exampleInputUsername2" class="col-sm-1 col-form-label">链接名称</label>
-                      <div class="col-sm-6">
-                        <input type="text" class="form-control" name="lname" id="exampleInputUsername2" placeholder="友情链接名称" value='{{ old("lname") }}'>
-                      </div>
+<div class="col-12 grid-margin">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">友情链接添加</h4>
+          <form class="form-sample" method="post" action="/admin/links">
+          {{ csrf_field() }}
+            <div class="row">
+              <div class="col-md-7">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">链接名称：</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="lname" />
+                  </div>
+                </div>
+              </div>                     
+            </div>
+            <div class="row">
+              <div class="col-md-7">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">链接URL：</label>
+                  <div class="col-sm-8">
+                    <input type="url" class="form-control" name="lurl" />
+                  </div>
+                </div>
+              </div>                     
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="form-group row">
+                  <label class="col-sm-2 col-form-label">状态：</label>
+                  <div class="col-sm-2">
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="0" checked>
+                       激活
+                      </label>
                     </div>
-                    <div class="form-group row">
-                      <label for="exampleInputEmail2" class="col-sm-1 col-form-label">链接URL</label>
-                      <div class="col-sm-6">
-                        <input type="url" class="form-control" name="lurl" id="exampleInputEmail2" placeholder="URL地址" value='{{ old("lurl") }}'>
-                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="status" id="membershipRadios2" value="1">
+                       未激活
+                      </label>
                     </div>
-                    <div class="form-group row">
-                      <label for="exampleInputEmail2" class="col-sm-1 col-form-label">状态</label>
-                      <div class="col-sm-1">
-                           <div class="form-check">
-                              <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="1" checked>
-                                激活
-                              </label>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                           <div class="form-check">
-                              <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="0">
-                                未激活
-                              </label>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-gradient-primary mr-2">添加</button>
-                    
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
+            <button type="submit" class="btn btn-gradient-primary mr-2">添加</button>
+          </form>
+        </div>
+      </div>
+    </div>
 @endsection
--- zhenyan/resources/views/admin/links/edit.blade.php
+++ zhenyan/resources/views/admin/links/edit.blade.php
@@ -1,47 +1,59 @@
 @extends('admin/layout/layout');
 @section('content-wrapper')
-<div class="col-md-10 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">友情链接修改</h4>
-                  <form class="forms-sample" action="/admin/links/{{$link->id}}" method="post">
-                  {{ csrf_field() }}
-                  {{ method_field('PUT') }}
-                    <div class="form-group row">
-                      <label for="exampleInputUsername2" class="col-sm-1 col-form-label">链接名称</label>
-                      <div class="col-sm-6">
-                        <input type="text" class="form-control" name="lname" id="exampleInputUsername2" placeholder="友情链接名称" value='{{ $link->lname }}'>
-                      </div>
+<div class="col-12 grid-margin">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">友情链接修改</h4>
+          <form class="form-sample" method="post" action="/admin/links/{{$link->id}}">
+          {{ csrf_field() }}
+          {{ method_field('PUT') }}
+            <div class="row">
+              <div class="col-md-7">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">链接名称：</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="form-control" name="lname" value='{{ $link->lname }}'/>
+                  </div>
+                </div>
+              </div>                     
+            </div>
+            <div class="row">
+              <div class="col-md-7">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">链接URL：</label>
+                  <div class="col-sm-8">
+                    <input type="url" class="form-control" name="lurl" value='{{ $link->lurl }}'/>
+                  </div>
+                </div>
+              </div>                     
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="form-group row">
+                  <label class="col-sm-2 col-form-label">状态：</label>
+                  <div class="col-sm-2">
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="0" @if ($link->status==0)checked @endif>
+                       激活
+                      </label>
                     </div>
-                    <div class="form-group row">
-                      <label for="exampleInputEmail2" class="col-sm-1 col-form-label">链接URL</label>
-                      <div class="col-sm-6">
-                        <input type="url" class="form-control" name="lurl" id="exampleInputEmail2" placeholder="URL地址" value='{{ $link->lurl }}'>
-                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                    <div class="form-check">
+                      <label class="form-check-label">
+                        <input type="radio" class="form-check-input" name="status" id="membershipRadios2" value="1" @if ($link->status==1)checked @endif>
+                       未激活
+                      </label>
                     </div>
-                    <div class="form-group row">
-                      <label for="exampleInputEmail2" class="col-sm-1 col-form-label">状态</label>
-                      <div class="col-sm-1">
-                           <div class="form-check">
-                              <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="1" @if ($link->status==1)checked @endif>
-                                激活
-                              </label>
-                            </div>
-                        </div>
-                        <div class="col-sm-4">
-                           <div class="form-check">
-                              <label class="form-check-label">
-                                <input type="radio" class="form-check-input" name="status" id="membershipRadios1" value="0" @if ($link->status==0)checked @endif>
-                                未激活
-                              </label>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-gradient-primary mr-2">修改</button>
-                    
-                  </form>
+                  </div>
                 </div>
               </div>
             </div>
+            <button type="submit" class="btn btn-gradient-primary mr-2">修改</button>
+          </form>
+        </div>
+      </div>
+    </div>
 @endsection
+
